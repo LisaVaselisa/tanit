6e53a9fa00a966507b14edcb73b282bbd4922115
LisaVaselisa <deoreczk@list.ru>
2020-05-24 18:19:38 +0100

    написала скрипт для меню

--- source/js/script.js
+++ source/js/script.js
@@ -3,11 +3,5 @@ var pageHeaderToggle = document.querySelector(".page-header__toggle");
 
 pageHeader.classList.remove("page-header__menu--nojs");
 pageHeaderToggle.addEventListener("click", function() {
-  if (pageHeader.classList.contains("page-header__menu--closed")) {
-    pageHeader.classList.remove("page-header__menu--closed");
-    pageHeader.classList.add("page-header__menu--opened");
-  } else {
-    pageHeader.classList.add("page-header__menu--closed");
-    pageHeader.classList.remove("page-header__menu--opened");
-  }
+  pageHeader.classList.toggle("page-header__menu--opened");
 });
